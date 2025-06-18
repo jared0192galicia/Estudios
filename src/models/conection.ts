e8c369d5bb11e49a1b7439a58a59d8c47581db9e
Jared Galicia <jared0192galicia@gmail.com>
2025-06-18 13:37:40 -0600

    cambiar ip

--- src/models/conection.ts
+++ src/models/conection.ts
@@ -1,6 +1,6 @@
 export const baseAPI =
   process.env.NODE_ENV == 'production'
-    ? 'https://132.18.53.133:3001'
+    ? '132.18.53.133:3001'
     : 'http://localhost:3001';
 
 export const environment =
